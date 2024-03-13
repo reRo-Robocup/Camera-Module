25f9154c5779dfca4b9485e164958de8a944d43c
onlydcx <rin.hanaoka@icloud.com>
2024-03-13 15:01:45 +0900

    整理

--- main.py
+++ main.py
@@ -32,11 +32,6 @@ Maix.utils.gc_heap_size(0x800000)
 print(Maix.utils.gc_heap_size())
 print(gc.mem_free())
 
-## 297 -> 45
-## 240 -> 38
-## 270 -> 45
-
-#sensor.reset(freq=29700000, set_regs=True, dual_buff=True)
 sensor.reset(freq=24000000, set_regs=True, dual_buff=True)
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
@@ -74,18 +69,12 @@ def getCam(threshold):
         cx_array.append(blob.cx())
         cy_array.append(blob.cy())
         enable = True
-        
-    del blob
-    gc.collect()
 
     max_pixels = max(pixels_array)
     index_id = pixels_array.index(max_pixels)
 
     cx = cx_array[index_id] - (mirror_cx)
     cy = cy_array[index_id] - (mirror_cy)
-    
-    del cx_array, cy_array
-    gc.collect()
 
     if cx == 0 and cy == 0:
         obj_angle = 361
@@ -121,7 +110,7 @@ def sendData(_ang_array, _distace_array, _enable_array):
 
     uart.write(enable.to_bytes(1, "little"))
 
-orange = [(48, 70, 46, 99, -97, 127)]
+orange = [(51, 70, 18, 52, 36, 127)]
 blue = [(0, 100, -118, 127, -111, -38)]
 yellow = [(0, 100, -128, 16, 39, 127)]
 
@@ -139,16 +128,13 @@ while True:
         enb_array = [ball_data[2], yell_data[2], blue_data[2]]
 
         sendData(ang_array, dis_array, enb_array)
-        
-        del ball_data, yell_data, blue_data, ang_array, dis_array, enb_array
-        gc.collect()
 
         #img.draw_line(int(mirror_cx),int(mirror_cy),int(ball_data[3] + mirror_cx),int(ball_data[4] + mirror_cy),(0, 0, 0),2,)
         #img.draw_line(int(mirror_cx),int(mirror_cy),int(blue_data[3] + mirror_cx),int(blue_data[4] + mirror_cy),(255, 255, 255),2,)
         #img.draw_line(int(mirror_cx),int(mirror_cy),int(yell_data[3] + mirror_cx),int(yell_data[4] + mirror_cy),(255, 255, 0),2,)
         #img.draw_cross(mirror_cx,mirror_cy,(255,255,255),5,2)
 
-        #print(blue_data[0])
+        #print(blue_data[1])
 
         #ang = blue_data[0] + 90
         #if(ang > 360):
@@ -157,7 +143,7 @@ while True:
         #blue_ang_rad = blue_data[0] * (math.pi / 180)
         #goal_xvect = abs(math.cos(blue_ang_rad) * 100)
         #print(goal_xvect)
-        print(clock.fps())
+        #print(clock.fps())
 
     except (OSError, RuntimeError, AttributeError) as err:
         # print(err)
